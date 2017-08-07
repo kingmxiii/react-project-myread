0823c4ac31f4e4b26d7ada9381c3bd094623419b
Rey Polanco <reypolanco@gmail.com>
2017-08-06 23:06:15 -0400

    feat: create BookShelf functional component

---
+++ src/BookShelf.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import BooksGrid from './BooksGrid'
+
+function BookShelf(props) {
+  return(
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.title}</h2>
+      <div className="bookshelf-books">
+          <BooksGrid bookList = {props.bookList} />
+      </div>
+    </div>
+  )
+
+}
+
+export default BookShelf
