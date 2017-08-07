b87fdbea7e67480686dfebdd0dd3d36fff5661fd
Rey Polanco <reypolanco@gmail.com>
2017-08-06 22:57:38 -0400

    feat: create BookGrid functional component

---
+++ src/BooksGrid.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+function BooksGrid(props) {
+  const coverStyle = {
+    width: 128,
+    height: 193,
+    backgroundImage: `url(${book.imageLinks.thumbnail})`
+  }
+  return (
+    <ol className="books-grid">
+      {props.bookList.map((book) => (
+        <li key={book.id}>
+          <div className="book">
+            <div className="book-top">
+              <div className="book-cover" style={coverStyle}>
+                  <div className="book-shelf-changer">
+                    <select>
+                      <option value="none" disabled>Move to...</option>
+                      <option value="currentlyReading">Currently Reading</option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
+              </div>
+              <div className="book-title">{book.title}</div>
+              <div className="book-authors">{book.authors.join(",")}</div>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ol>
+  )
+}
+
+export default BooksGrid
